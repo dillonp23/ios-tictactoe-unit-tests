247c4793331549ea83567f159f60c0d6857c44b0
Dillon <dillonp.lambda@gmail.com>
2019-11-20 20:35:53 -0800

    Update accessibility identifiers in storyboard. Implement UI Test to check for a win.

---
+++ TicTacToeUITests/TicTacToeUITests.swift
@@ -0,0 +1,94 @@
+//
+//  TicTacToeUITests.swift
+//  TicTacToeUITests
+//
+//  Created by dillon on 11/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import XCTest
+
+class TicTacToeUITests: XCTestCase {
+    
+    func testLeftVertical() {
+        
+        /// D.R.Y. - Don't Repeat Yourself - whenever you see code and wnat to copy it, dont!! -> use a function!
+        
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        // X starts game
+        app.assertMovePlayed(.topLeft, buttonLabelAssertionString: .X, nextPlayerAssertionString: .O)
+        app.assertMovePlayed(.topRight, buttonLabelAssertionString: .O, nextPlayerAssertionString: .X)
+        app.assertMovePlayed(.middleLeft, buttonLabelAssertionString: .X, nextPlayerAssertionString: .O)
+        app.assertMovePlayed(.middleRight, buttonLabelAssertionString: .O, nextPlayerAssertionString: .X)
+        
+        // X wins
+        let bottomLeft = app.buttons["bottomLeft"]
+        bottomLeft.tap()
+        XCTAssertEqual(bottomLeft.label, "X")
+        XCTAssertEqual(app.staticTexts["statusLabel"].label, "Player X won!")
+    }
+    
+    
+    func testDiagonalWin() {
+        
+        /// D.R.Y. - Don't Repeat Yourself - whenever you see code and wnat to copy it, dont!! -> use a function!
+        
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+        
+        // X starts game
+        app.assertMovePlayed(.topLeft, buttonLabelAssertionString: .X, nextPlayerAssertionString: .O)
+        app.assertMovePlayed(.bottomLeft, buttonLabelAssertionString: .O, nextPlayerAssertionString: .X)
+        app.assertMovePlayed(.middleCenter, buttonLabelAssertionString: .X, nextPlayerAssertionString: .O)
+        app.assertMovePlayed(.topRight, buttonLabelAssertionString: .O, nextPlayerAssertionString: .X)
+        
+        // X wins
+        let bottomRight = app.buttons["bottomRight"]
+        bottomRight.tap()
+        XCTAssertEqual(bottomRight.label, "X")
+        XCTAssertEqual(app.staticTexts["statusLabel"].label, "Player X won!")
+        
+    }
+    
+    
+    
+}
+
+
+
+extension XCUIApplication {
+    
+    var statusElement: XCUIElement {
+        return staticTexts["statusLabel"]
+    }
+    
+    func assertMovePlayed(_ buttonIdentifier: PositionIdentifier,
+                          buttonLabelAssertionString: ButtonLabel,
+                          nextPlayerAssertionString: ButtonLabel) {
+        let button = buttons[buttonIdentifier.rawValue]
+        button.tap()
+        XCTAssertEqual(button.label, buttonLabelAssertionString.rawValue)
+        XCTAssertEqual(statusElement.label, "Player \(nextPlayerAssertionString.rawValue)'s turn")
+    }
+    
+    enum PositionIdentifier: String {
+        case topLeft
+        case topCenter
+        case topRight
+        case middleLeft
+        case middleCenter
+        case middleRight
+        case bottomLeft
+        case bottomCenter
+        case bottomRight
+    }
+    
+    enum ButtonLabel: String {
+        case X
+        case O
+    }
+}
